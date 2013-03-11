c6b6d1215e39e717f5b2ac24c3113059aec1db86
Dimo Hubenov <dimexer@debian.workgroup>
2013-03-11 23:51:49 +0200

    added solution to chall01 + tests

---
+++ test/clojure_challenges/chall01/sample_test.clj
@@ -0,0 +1,14 @@
+(ns clojure-challenges.chall01.sample-test
+  (:use clojure.test))
+
+(load-file "src/clojure_challenges/chall01/challenge.clj")
+
+(deftest sample-test
+  (testing "histogram"
+      (is [1 2 3] (histogram [0 1 1 2 2 2]))
+      (is [1] (histogram [0]))
+      (is [4 0 0 2] (histogram [0 0 0 0 3 3]))
+      (is [0 1 1 1] (histogram [1 2 3]))
+      (is (vec (repeat 1000001 1)) (histogram (range 0 1000001)))))
+
+(run-tests)
--- test/clojure_challenges/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-challenges.core-test
-  (:use clojure.test
-        clojure-challenges.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
