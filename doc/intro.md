55f576729e28e1c75219016cbc1de3f62838a6f4
Dimo Hubenov <dimexer@debian.workgroup>
2013-03-11 21:55:51 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-challenges
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_challenges/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-challenges.core-test
+  (:use clojure.test
+        clojure-challenges.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
