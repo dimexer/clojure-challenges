be68d503a3fea4a72ba098136e96d780066d4467
Dimo Hubenov <dimexer@debian.workgroup>
2013-03-12 00:04:18 +0200

    removed unnecessary comments

--- src/clojure_challenges/chall01/challenge.clj
+++ src/clojure_challenges/chall01/challenge.clj
@@ -2,9 +2,3 @@
   (let [occurs (group-by identity xs)]
     (vec (for [x (range 0 (+ (last xs) 1))]
       (if (nil? (occurs x)) 0 (count (occurs x)))))))
-
-;(println (histogram [0 0 0 1 1 3 3]))
-;(println "-----------------")
-;(println (histogram [0]))
-;(println "-----------------")
-;(println (histogram [0 0 0 0 0 0 10 10 10 10]))
