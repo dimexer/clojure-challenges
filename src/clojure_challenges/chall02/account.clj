1734e98a4d1904881b8de91b0d1b5e9798b495b4
Dimo Hubenov <dimexer@debian.workgroup>
2013-03-27 23:22:16 +0200

    code formatter + line representing proper exception throwing

--- src/clojure_challenges/chall02/account.clj
+++ src/clojure_challenges/chall02/account.clj
@@ -1,4 +1,4 @@
-(defn make-account[] (atom 0 :validator integer?))
+(defn make-account [] (atom 0 :validator integer?))
 
 (defn balance [acc] (deref acc))
 
@@ -7,3 +7,4 @@
 (defn withdraw [acc amt] (swap! acc - amt) acc)
 
 ;(println (-> (make-account) (deposit 10) (withdraw 6) balance))
+;(println (-> (make-account) (deposit 10) (withdraw 0.5) balance))
